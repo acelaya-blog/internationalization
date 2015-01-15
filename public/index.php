7c68ff727a5c2f488534a5e2cad8cc2117a4ce94
Alejandro Celaya <alejandro@alejandrocelaya.com>
2015-01-15 09:54:07 +0100

    Enhanced HTML base

--- public/index.php
+++ public/index.php
@@ -1,11 +1,22 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 include __DIR__ . '/../vendor/autoload.php';
 
 $lang = isset($_GET['lang']) ? $_GET['lang'] : 'en';
 /** @var Zend\I18n\Translator\Translator $translator */
 $translator = include __DIR__ . '/../src/translator.php';
 $translator->setLocale($lang);
-
-echo sprintf('<p>%s</p>', $translator->translate('Goodbye my friend'));
-echo sprintf('<p>%s</p>', $translator->translate('How are you?'));
-echo sprintf('<p>%s</p>', $translator->translate('My name is John Doe'));
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<p><?php echo $translator->translate('Goodbye my friend'); ?></p>
+		<p><?php echo $translator->translate('How are you?'); ?></p>
+		<p><?php echo $translator->translate('My name is John Doe'); ?></p>
+	</body>
+</html>
