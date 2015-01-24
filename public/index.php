1df3775628d5b1fcaece2304581a5b4df2c12062
Alejandro Celaya <alejandro@alejandrocelaya.com>
2015-01-24 13:09:22 +0100

    Updated translations

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@ $translator->setLocale($lang);
         <meta charset="utf-8">
     </head>
     <body>
-        <p><?php echo $translator->translate('Goodbye my friend'); ?></p>
+        <p><?php echo $translator->translate('Hello my friend'); ?></p>
         <p><?php echo $translator->translate('How are you?'); ?></p>
         <p><?php echo $translator->translate('My name is John Doe'); ?></p>
     </body>
